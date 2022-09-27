fe4cd97b663ccc24d3bc6ae678e949de57f9440a
EDUARDO PAIXÃO DOS SANTOS SEHN <eduardo_sehn@estudante.sesisenai.org.br>
2022-09-27 12:52:20 -0300

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,114 @@ import './App.css';
 function App() {
 
    const [file, setFile] = useState();
+   const [user, setUser] = useState({
+      "fullname": "",
+      "username": "",
+      "email": "",
+      "password": "",
+      "born": "",
+   })
 
+
+   const User = {
+      "fullname": "",
+      "username": "",
+      "email": "",
+      "password": "",
+      "born": "",
+  
+   }
 
   const getFileHandler = (event)=>{
-     setFile(event.target.files[0])
-  }
-
-  const saveImage = async ()=>{
-
-    let formData = new FormData();
-
-    formData.append("file", file);
+   let formData = new FormData();
+    setFile(event.target.files[0])
+    formData.append("file");
     formData.append("fileName", file.name)
-    
-  
-     const response = await fetch("http://localhost:8080/user/saveUserImage", {
-      mode: "no-cors",  
-       method: "POST",
-       body: formData
-       
-       
-       
-       
-     })
+
+     User.photo = formData;
   }
+
+  const handlerUserFullname = (event)=>{
+      setUser({"fullname": event})
+      User.fullname = event;
+  }
+
+  const handlerUserUsername = (event)=>{
+   setUser({"username": event})
+    User.username = event;
+ }
+
+ const handlerUserEmail = (event)=>{
+   setUser({"email": event})
+   User.email = event;
+}
+
+const handlerUserPassword = (event)=>{
+   setUser({"password": event})
+   User.password = event;
+}
+
+const handlerUserBorn = (event)=>{
+   setUser({"born": event})
+   User.born = event;
+}
+
+  const createUser = async ()=>{
+     const response = await fetch("http://localhost:8080/user/saveUserImage", {
+       mode: "no-cors",  
+       method: "POST",
+       body: JSON.stringify(User)
+     })
+
+     if (response == "true"){
+        alert("FOI")
+     }
+  }
+
+  const handlerUser = (User)=>{
+      createUser();
+ }
 
 
   return (
     <div className="App">
       <h1>FILE UPLOAD TEST</h1>
-       <input type="file"  onChange={
-          getFileHandler
+       <label>Nome Completo</label>
+       <input type="text" value={user.fullname} onChange={
+           (e)=>handlerUserFullname(e.target.value)
        }  />
-       <button onClick={saveImage} >SEND FILE</button>
+       <br />
+       <br />
+        <label>Username</label>
+       <input type="text" value={user.username} onChange={
+           (e)=>handlerUserUsername(e.target.value)
+       }  />
+         <br />
+         <br />
+        <label>Email</label>
+       <input type="text" value={user.email} onChange={
+           (e)=>handlerUserEmail(e.target.value)
+       }  />
+     <br />
+     <br />
+     <label>Senha</label>
+       <input type="text" value={user.password} onChange={
+           (e)=>handlerUserPassword(e.target.value)
+       }  />
+     <br />
+     <br />
+     <label>Nacimento</label>
+       <input type="text" value={user.born} onChange={
+           (e)=>handlerUserBorn(e.target.value)
+       }  />
+     <br />
+     <br />
+       <input type="file"  onChange={
+          getFileHandler   
+       }  />
+         <br />
+         <br />
+       <button onClick={handlerUser(User)} >CREATE A USER</button>
     </div>
   );
 }
